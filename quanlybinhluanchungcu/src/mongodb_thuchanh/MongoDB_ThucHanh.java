65c110f0591a78c885e1b9189c89413f69b69595
𝗡 𝗢 𝐆 𝐋 𝐎 𝐑 𝐘 ✰ <btooom1999@gmail.com>
2020-10-02 16:46:53 +0700

    first commit

---
+++ quanlybinhluanchungcu/src/mongodb_thuchanh/MongoDB_ThucHanh.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mongodb_thuchanh;
+
+import Frame.BinhLuanGopYChungCu;
+
+/**
+ *
+ * @author btooo
+ */
+public class MongoDB_ThucHanh {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        BinhLuanGopYChungCu f = new BinhLuanGopYChungCu();
+        f.setVisible(true);
+    }
+    
+}
